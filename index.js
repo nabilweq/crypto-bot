547f7226df6244ffadc6f9438a7addf841a2c8a3
nabilweq <nabeeltkanr@gmail.com>
2022-01-08 07:49:03 +0530

    Fixed the heroku error

--- index.js
+++ index.js
@@ -5,133 +5,133 @@ const app = express();
 const { Telegraf }  = require('telegraf');
 const axios = require('axios');
 const cors = require("cors");
-const token = process.env.BOT_TOKEN;
-const bot = new Telegraf(token);
+// const token = process.env.BOT_TOKEN;
+// const bot = new Telegraf(token);
 
-const apiKey = process.env.API_KEY;
+// const apiKey = process.env.API_KEY;
 
 app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello Bro!');
 })
 
-bot.command('start', ctx => {
-    sendStartMessage(ctx)
-});
+// bot.command('start', ctx => {
+//     sendStartMessage(ctx)
+// });
 
-bot.action('start', ctx => {
-    ctx.deleteMessage();
-    sendStartMessage(ctx)
-});
+// bot.action('start', ctx => {
+//     ctx.deleteMessage();
+//     sendStartMessage(ctx)
+// });
 
-function sendStartMessage(ctx) {
-    let startMessage = "Welcome, this bot gives you crypto currency information";
-    bot.telegram.sendMessage(ctx.chat.id, startMessage,
-    {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {text: "Crypto Prices", callback_data: "price"}
-                ],
-                [
-                    {text: "CoinMarketcap", url: "https://www.cryptocompare.com/"}
-                ]
-            ]
+// function sendStartMessage(ctx) {
+//     let startMessage = "Welcome, this bot gives you crypto currency information";
+//     bot.telegram.sendMessage(ctx.chat.id, startMessage,
+//     {
+//         reply_markup: {
+//             inline_keyboard: [
+//                 [
+//                     {text: "Crypto Prices", callback_data: "price"}
+//                 ],
+//                 [
+//                     {text: "CoinMarketcap", url: "https://www.cryptocompare.com/"}
+//                 ]
+//             ]
 
-        }
-    });
-}
+//         }
+//     });
+// }
 
-bot.action('price', ctx => {
-    let priceMessage = "Get Price Information. Select one of the cryptocurrencies below";
-    ctx.deleteMessage();
-    bot.telegram.sendMessage(ctx.chat.id, priceMessage,
-        {
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        {text: "BTC", callback_data: "price-BTC"},
-                        {text: "ETH", callback_data: "price-ETH"},
-                    ],
-                    [
-                        {text: "BCH", callback_data: "price-BCH"},
-                        {text: "LTC", callback_data: "price-LTC"},
-                    ],
-                    [
-                        {text: "Back to Menu", callback_data: "start"}
-                    ]
-                ]
-            }
-        });
-});
+// bot.action('price', ctx => {
+//     let priceMessage = "Get Price Information. Select one of the cryptocurrencies below";
+//     ctx.deleteMessage();
+//     bot.telegram.sendMessage(ctx.chat.id, priceMessage,
+//         {
+//             reply_markup: {
+//                 inline_keyboard: [
+//                     [
+//                         {text: "BTC", callback_data: "price-BTC"},
+//                         {text: "ETH", callback_data: "price-ETH"},
+//                     ],
+//                     [
+//                         {text: "BCH", callback_data: "price-BCH"},
+//                         {text: "LTC", callback_data: "price-LTC"},
+//                     ],
+//                     [
+//                         {text: "Back to Menu", callback_data: "start"}
+//                     ]
+//                 ]
+//             }
+//         });
+// });
 
-let priceActionList = ["price-BTC", "price-ETH", "price-BCH", "price-LTC"];
-bot.action(priceActionList, async ctx => {
+// let priceActionList = ["price-BTC", "price-ETH", "price-BCH", "price-LTC"];
+// bot.action(priceActionList, async ctx => {
 
-    let symbol = ctx.match.input.split('-')[1];
+//     let symbol = ctx.match.input.split('-')[1];
 
-    try {
+//     try {
 
-        let res = await axios.get(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${symbol}&tsyms=USD&api_key=${apiKey} `)
+//         let res = await axios.get(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${symbol}&tsyms=USD&api_key=${apiKey} `)
 
-        let data = res.data.DISPLAY[symbol].USD;
+//         let data = res.data.DISPLAY[symbol].USD;
 
-        let message = 
-        `
-Symbol:  ${symbol}
-Price:  ${data.PRICE}
-Open:  ${data.OPENDAY}
-High:  ${data.HIGHDAY}
-Low:  ${data.LOWDAY}
-Supply:  ${data.SUPPLY}
-Market Cap:  ${data.MKTCAP}
-`;
+//         let message = 
+//         `
+// Symbol:  ${symbol}
+// Price:  ${data.PRICE}
+// Open:  ${data.OPENDAY}
+// High:  ${data.HIGHDAY}
+// Low:  ${data.LOWDAY}
+// Supply:  ${data.SUPPLY}
+// Market Cap:  ${data.MKTCAP}
+// `;
 
-        ctx.deleteMessage();
-        bot.telegram.sendMessage(ctx.chat.id, message, 
-        {
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        {text: "Back to Prices", callback_data: "price"}
-                    ]
-                ]
+//         ctx.deleteMessage();
+//         bot.telegram.sendMessage(ctx.chat.id, message, 
+//         {
+//             reply_markup: {
+//                 inline_keyboard: [
+//                     [
+//                         {text: "Back to Prices", callback_data: "price"}
+//                     ]
+//                 ]
     
-            }
-        });
+//             }
+//         });
 
-    } catch (err) {
-        ctx.reply("Error Encountered");
-        console.log(err.message);
-    }
-    //https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=IND
-});
+//     } catch (err) {
+//         ctx.reply("Error Encountered");
+//         console.log(err.message);
+//     }
+//     //https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=IND
+// });
 
-bot.command('info', ctx => {
-    bot.telegram.sendMessage(ctx.chat.id, "Bot Info", 
-    {
-        reply_markup: {
-            keyboard: [
-                [
-                    {text: "Credits"},
-                    {text: "API"}
-                ]
-            ],
-            resize_keyboard: true,
-            one_time_keyboard: true
-        }
-    });
-});
+// bot.command('info', ctx => {
+//     bot.telegram.sendMessage(ctx.chat.id, "Bot Info", 
+//     {
+//         reply_markup: {
+//             keyboard: [
+//                 [
+//                     {text: "Credits"},
+//                     {text: "API"}
+//                 ]
+//             ],
+//             resize_keyboard: true,
+//             one_time_keyboard: true
+//         }
+//     });
+// });
 
-bot.hears('Credits', ctx => {
-    ctx.reply("This bot was made by Nabeel");
-});
+// bot.hears('Credits', ctx => {
+//     ctx.reply("This bot was made by Nabeel");
+// });
 
-bot.hears('API', ctx => {
-    ctx.reply("This bot uses cryptocompare API");
-});
+// bot.hears('API', ctx => {
+//     ctx.reply("This bot uses cryptocompare API");
+// });
 
-bot.launch();
+// bot.launch();
 
 app.listen(3000, ()=> {
     console.log("server started");
